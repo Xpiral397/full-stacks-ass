81a21eea767b8f240960810b9cd00312e2dd2e24
Xpiral397 <111965825+Xpiral397@users.noreply.github.com>
2024-08-24 06:40:50 +0100

    Deploy

--- backend/menu_management/menus/models.py
+++ backend/menu_management/menus/models.py
@@ -1,16 +1,23 @@
-# menus/models.py
-
 from django.db import models
+
+
 import uuid
 
-class MenuItem(models.Model):
+
+class MenuNode(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
-    parent = models.ForeignKey('self', null=True, blank=True, related_name='children', on_delete=models.CASCADE)
-    depth = models.PositiveIntegerField(default=0)  # to keep track of hierarchical level
+    parent = models.ForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        related_name="childrens",
+        null=True,
+        blank=True,
+    )
+    depth = models.PositiveIntegerField()
+    children = models.JSONField(
+        default=list, blank=True
+    )  # Store children as a JSON list
 
     def __str__(self):
         return self.name
-
-    class Meta:
-        ordering = ['depth', 'name']

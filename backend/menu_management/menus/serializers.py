1c70993e87c563bb67ec179a3c7cbaa04e9e662c
xpiral <111965825+Xpiral397@users.noreply.github.com>
2024-08-24 07:06:03 +0100

    Delete backend/menu_management directory

--- backend/menu_management/menus/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import MenuNode
-
-
-class MenuNodeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MenuNode
-        fields = ["id", "name", "parent", "depth", "children"]
